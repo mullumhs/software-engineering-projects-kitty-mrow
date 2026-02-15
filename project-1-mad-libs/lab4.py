85ef041a9ddd295fcf9ac0dfefc3db9633f5412d
kitty-mrow <josh.griffith@education.nsw.gov.au>
2026-02-16 09:45:14 +1100

    lab 4 done

--- project-1-mad-libs/lab4.py
+++ project-1-mad-libs/lab4.py
@@ -6,3 +6,6 @@
 - Class: Software Engineering
 - Description: Ask for full name and greet user by their first name only.
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+fullName=input("what is youre full name?\n")
+name=fullName.strip().title().split()
+print("hello",name[0]+"!")
